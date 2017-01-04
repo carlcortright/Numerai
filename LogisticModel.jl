e467e2077185e4ed0a4fe21716341823a8dc7a9e
Carl Cortright <ckcortright@gmail.com>
2017-01-04 14:26:41 -0600

    fixed by using full path from root

--- LogisticModel.jl
+++ LogisticModel.jl
@@ -10,6 +10,6 @@ using DataFrames, DataArrays
 
 @sk_import linear_model: LogisticRegression
 
-train = readtable("training")
+train = readtable("/home/carl/Documents/Numerai/Datasets/Jan32017/training.csv")
 
 println("done")
