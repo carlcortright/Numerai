e3516f4038a1182071852cfdd91b6ccd2569947d
Carl Cortright <ckcortright@gmail.com>
2017-01-04 14:20:58 -0600

    Trying to fix issue with dataset

--- LogisticModel.jl
+++ LogisticModel.jl
@@ -5,12 +5,11 @@
 # Date: 1/4/2017
 #
 ===============================================================================#
-workspace()
 using ScikitLearn
 using DataFrames, DataArrays
 
 @sk_import linear_model: LogisticRegression
 
-train = readtable("Datasets/Jan32017/training.csv")
+train = readtable("training")
 
 println("done")
