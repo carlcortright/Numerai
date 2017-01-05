9937bb809ab78276e1523b8dacf0a572d8ba7ce4
Carl Cortright <ckcortright@gmail.com>
2017-01-04 20:11:26 -0600

    After learning lots about dataframes in julia, logreg is not the best approach for numerai

--- LogisticModel.jl
+++ LogisticModel.jl
@@ -9,7 +9,19 @@ using ScikitLearn
 using DataFrames, DataArrays
 
 @sk_import linear_model: LogisticRegression
+@sk_import preprocessing: (LabelBinarizer, StandardScaler)
 
+# Define he training set
+features = [symbol("feature$i") for i in 1:50]
 train = readtable("/home/carl/Documents/Numerai/Datasets/Jan32017/training.csv")
 
-println("done")
+# Pull out features and targets
+features = convert(Array, train[1:50])
+targets = convert(Array, train[51])
+
+# Fit a logistic regression
+logreg = LogisticRegression(fit_intercept=true)
+ScikitLearn.fit!(logreg, features, targets)
+
+accuracy = sum(ScikitLearn.predict(logreg, features) .== targets) / length(targets)
+println("accuracy: $accuracy")
